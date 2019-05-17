581c9d2a4ce8677e46253b3dd467c7f9d367857f
yegheliy_aleksey <yegheliyaleksey@gmail.com>
2019-05-17 21:02:19 +0300

    Правка контроллера BachelorAcceleratedController

---
+++ app/Admin/Controllers/BachelorAcceleratedController.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\BachelorАccelerated;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+use Illuminate\Support\MessageBag;
+
+class BachelorАcceleratedController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('Бакалавр ускор.')
+            ->description(' ')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Просмотр записи')
+            ->description(' ')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Редактирование записи')
+            ->description(' ')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Создание записи')
+            ->description(' ')
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new BachelorАccelerated);
+        $grid->id('Ид');
+        $grid->title('Заголовок')->sortable();
+        $grid->content('Содежание');
+        $grid->image('Изображение')->display(function ($id) {
+            if (isset($id)) {
+                return '<a href="/admin/media?path=' . urlencode('/uploads/' . $id) . '">Просмотреть</a>';
+            } else {
+                return '-';
+            }
+        });
+        $grid->is_public('Публичная новость')->display(function ($isPublic) {
+            return $isPublic ? 'Да' : 'Нет';
+        })->sortable();
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(BachelorАccelerated::findOrFail($id));
+
+        $show->id('Ид');
+        $show->title('Заголовк');
+        $show->content('Содежание')->unescape();
+        $show->image('Иллюстрация')->image();
+        $show->is_public('Публичная новoсть')->using([1 => 'Да', 0 => 'Нет']);
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new BachelorАccelerated);
+
+        $form->text('title', 'Заголовок')->rules('required|max:255', [
+            'required' => 'Обязательно для заполнения',
+            'max' => 'Кол-во символов не более :max',
+        ]);;
+        $form->textarea('content', 'Содержание')->rules('required|max:30000', [
+            'required' => 'Обязательно для заполнения',
+            'max' => 'Кол-во символов не более :max',
+        ]);
+        $form->image('image', 'Изображение')->rules('image', [
+            'image' => 'Это должна быть картинка',
+        ]);
+        $form->switch('is_public', 'Публичная новсть')->default(1);
+
+
+        $form->saved(function (Form $form) {
+            $path = storage_path('/app/public/uploads' . $form->model()->id);
+            dump($path);
+            if(!file_exists($path)) {
+                mkdir($path);
+            }   
+        });
+
+        $form->saving(function ($form) {
+            $result = BachelorАccelerated::where("is_public", "=", 1)->count();
+
+            /* определение числа рядов в выборке */
+            if ($result >= 5 && $form->is_public === 'on') {
+                // redirect back with an error message
+                $error = new MessageBag([
+                    'title'   => 'Ошибка',
+                    'message' => 'Максимальное количество активных записей 5. ',
+                ]);
+
+                return redirect('/admin/telegram-bot/campaign/bachelor-accelerated')->with(compact('error'));
+            }
+        });
+
+        return $form;
+    }
+}
+
