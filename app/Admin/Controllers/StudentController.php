640457ee758630cfc95a207c7454175ea9243fd9
Tarasenko Evgenii <evgenii.tarasenko@uadevelopers.com>
2019-11-13 17:38:23 +0200

    FIX

--- app/Admin/Controllers/StudentController.php
+++ app/Admin/Controllers/StudentController.php
@@ -132,7 +132,7 @@ class StudentController extends Controller
         $form->text('surname', 'Фамилия')->rules('required|max:50');
         $form->text('name', 'Имя')->rules('required|max:50');
         $form->text('family_name', 'Отчество')->rules('required|max:255');
-        $form->text('telegram_id', 'Telegram-id')->rules('nullable|unique:students');
+        $form->text('telegram_id', 'Telegram-id')->rules('nullable');
         $form->email('email', 'Email')->rules('unique:students');
         $form->text('number', 'Контактный телефон')->rules('max:100');;
         $form->select('groups_id', 'Ид группы')->options(Group::all()->pluck('name', 'id'))
